59f99418aaaeac514f49b572262a186af8c34f0f
soncoerok <Guillaume@10.143.45.2>
2016-10-05 20:27:16 +0200

    Change parameters

--- app/src/main/java/com/example/nantes/mines/myChat/utils/Data.java
+++ app/src/main/java/com/example/nantes/mines/myChat/utils/Data.java
@@ -9,24 +9,24 @@ public class Data {
     /** The password used by the user */
     private static String pass = "";
     /** The URL used to test if a user is known */
-    private final static String CONNECT_URL = "http://training.loicortola.com/parlez-vous-android/connect/";
+    private final static String CONNECT_URL = "127.0.0.1/api/connect/";
     /** The URL used to register an user */
-    private final static String REGISTER_URL = "http://training.loicortola.com/parlez-vous-android/register/";
+    private final static String REGISTER_URL = "127.0.0.1/api/register/";
     /** The URL used to get all the message */
-    private final static String GET_MESSAGE_URL = "http://training.loicortola.com/parlez-vous-android/message/";
+    private final static String GET_MESSAGE_URL = "127.0.0.1/api/message/";
     /** The URL used to send a message */
-    private final static String SEND_MESSAGE_URL = "http://training.loicortola.com/parlez-vous-android/message/";
+    private final static String SEND_MESSAGE_URL = "127.0.0.1/api/message/";
     /** The name of the sharedPreferences */
     private final static String PREFERENCES = "MyPrefs";
     /** The sharedPreferences name for a login */
-    private final static String PREF_LOGIN = "Login";
+    private final static String PREF_LOGIN = "login";
     /** The sharedPreferences name for a password */
-    private final static String PREF_PASS = "Pass";
+    private final static String PREF_PASS = "pass";
     /** The name used to save the status of the error text in the login activity */
     private final static String NAME_STATE_LOGIN_ERROR = "errorState";
     /** The name used to save the position of the listview in the main activity */
     private final static String NAME_STATE_LIST = "listState";
-    /** The message to send to open the easter egg (drawActivity */
+    /** The message to send to open the easter egg (drawActivity) */
     private final static String NAME_EASTER_EGG_DRAW = "#picasso";
 
     public static String getLogin() {
